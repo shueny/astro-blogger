6c826a4ee2d7a01a5b87a51abb6d3f223b82d8f8
Shueny Wang <shuenywang@Shuenydgdiannao.Askey.com>
2024-07-21 11:19:52 +0800

    update site settings

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,43 +1,43 @@
-import type { SidebarNavItem, SiteConfig } from "@/types";
+import type {SidebarNavItem, SiteConfig} from '@/types';
 
 export const siteConfig: SiteConfig = {
-  name: "Astronomy",
-  description:
-    "An modern example app built using Astro v4 & shadcn/ui. Inspired by shadcn/taxonomy.",
-  url: "https://astro-nomy.vercel.app",
-  ogImage: "https://astro-nomy.vercel.app/og.jpg",
-  links: {
-    twitter: "https://twitter.com/miickasmt",
-    github: "https://github.com/mickasmt/astro-nomy",
-  },
+	name: 'Shueny',
+	description:
+		'An modern example app built using Astro v4 & shadcn/ui. Inspired by shadcn/taxonomy.',
+	url: 'https://astro-nomy.vercel.app',
+	ogImage: 'https://astro-nomy.vercel.app/og.jpg',
+	links: {
+		instagram: 'https://www.instagram.com/shueny_wang/',
+		github: 'https://github.com/shueny',
+	},
 };
 
 export const footerLinks: SidebarNavItem[] = [
-  {
-    title: "Company",
-    items: [
-      { title: "About", href: "#" },
-      { title: "Enterprise", href: "#" },
-      { title: "Partners", href: "#" },
-      { title: "Jobs", href: "#" },
-    ],
-  },
-  {
-    title: "Product",
-    items: [
-      { title: "Security", href: "#" },
-      { title: "Customization", href: "#" },
-      { title: "Customers", href: "#" },
-      { title: "Changelog", href: "#" },
-    ],
-  },
-  {
-    title: "Docs",
-    items: [
-      { title: "Introduction", href: "#" },
-      { title: "Installation", href: "#" },
-      { title: "Components", href: "#" },
-      { title: "Code Blocks", href: "#" },
-    ],
-  },
+	{
+		title: 'Company',
+		items: [
+			{title: 'About', href: '#'},
+			{title: 'Enterprise', href: '#'},
+			{title: 'Partners', href: '#'},
+			{title: 'Jobs', href: '#'},
+		],
+	},
+	{
+		title: 'Product',
+		items: [
+			{title: 'Security', href: '#'},
+			{title: 'Customization', href: '#'},
+			{title: 'Customers', href: '#'},
+			{title: 'Changelog', href: '#'},
+		],
+	},
+	{
+		title: 'Docs',
+		items: [
+			{title: 'Introduction', href: '#'},
+			{title: 'Installation', href: '#'},
+			{title: 'Components', href: '#'},
+			{title: 'Code Blocks', href: '#'},
+		],
+	},
 ];
